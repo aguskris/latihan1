da0d569e306dfdf2eecb74e716b39710443edd2a
agus <agusabad2000@gmail.com>
2018-03-27 14:21:19 +0700

    membuat class baru

---
+++ latihan1/buku.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan1;
+
+/**
+ *
+ * @author user
+ */
+public class buku {
+    String Cover;
+    String Halaman;
+    String DaftarIsi;
+    String Bab;
+    String Pengarang;
+    
+    void infobuku(){
+        System.out.println("Cover        :"+Cover);
+        System.out.println("Halaman      :"+Halaman);
+        System.out.println("DaftarIsi    :"+DaftarIsi);
+        System.out.println("Bab          :"+Bab);
+        System.out.println("Pengarang    :"+Pengarang);
+        
+    }
+
+}
